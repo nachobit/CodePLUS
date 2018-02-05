939008a15b847f267f2d377498e5afbf2f676a29
Nacho Romero <nachotempus@gmail.com>
2018-02-05 23:06:52 +0100

    update inter code

--- ED/Resueltos/iterador_res.cpp
+++ ED/Resueltos/iterador_res.cpp
@@ -8,4 +8,63 @@ void escribir_elementos(const T & contenedor){
 		cout << *p << endl;
 }
 
+typedef double * iterator;
 
+iterator begin(double *v, int n){
+	return v;
+}
+
+iterator end(double *v, int n){
+	return v+n;
+}
+
+void anular_elementos(double *v, int n){
+	iterator p;
+	for(p=begin(v,n); p!=end(v,n); ++p)
+		*p==0;
+}
+
+void escribir_elementos(const double *v, int n){
+	const_iterator p;
+	for(p=begin(v,n); p!=end(v,n); ++p)
+		cout << *p << endl;
+}
+
+
+/**/
+#include <iostream>
+#include <vector.h>
+#include <cassert>
+#include <string>
+using namespace std;
+
+void cargar_indices(vector<int> & v){
+	for(int i=0; i<v.size(); ++i)
+		v[i]=i;
+}
+
+template <class T> T maximo(const vector<T> & v){
+	assert (v.begin()!=v.end());	//assert: compare to 0
+	typename vector<T>::const_iterator max = v.begin();
+	for(vector<T>::const_iterator p=v.begin(); p!=v.end();++p){
+		if(*max < *p)
+			max=p;
+	}
+	return *max;
+}
+
+int main(){
+	vector<int> vec(3);
+	vector<string>cadenas(4);
+	cargar_indices(vec);
+	cout <<"Maximo de" << vec.size() << " elementos enteros: " << maximo(vec) << endl;
+	vec.resize(10);
+	cargar_indices(vec);
+	cout <<"Maximo de" << vec.size() << " elementos enteros: " << maximo(vec) << endl;
+	cadenas[0] = "Esto";
+	cadenas[1] = "es";
+	cadenas[2] = "una";
+	cadenas[3] = "prueba";
+	cout <<"Maximo de" << cadenas.size() << " elementos cadena: " << maximo(cadenas) << endl;
+	return 0;
+}
